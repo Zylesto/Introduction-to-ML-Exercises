345153d6b8d92c8c48fb8bd99de51c3ce7abc8ad
Felix Berger <felix.berger98@icloud.com>
2023-05-11 21:42:41 +0200

    Solved exercise 3

--- introml_ex1/exercise_1_piano.py
+++ introml_ex1/exercise_1_piano.py
@@ -1,19 +1,28 @@
-from math import ceil
-import matplotlib.pyplot as plt
 import numpy as np
-import os
-from tqdm import tqdm
 
 def load_sample(filename, duration=4*44100, offset=44100//10):
-    # Complete this function
-    return None
+    sample = np.load(filename)
+    highest_value = np.argmax(np.abs(sample))
+    start_pos = highest_value + offset
+    sliced_sample = sample[start_pos:start_pos + duration]
+    return sliced_sample
 
 def compute_frequency(signal, min_freq=20):
-    # Complete this function
-    return 0
+
+    fft = np.fft.fft(signal)
+    fft = np.where(fft < min_freq, 0, fft)
+
+    magnitude = np.abs(fft)
+    frequency = np.fft.fftfreq(len(signal))
+    highest_peak = np.argmax(magnitude)
+
+    return frequency[highest_peak] * len(signal) * (1 / (len(signal) / 4*44100)) # Abtastrate = len(signal) (Anzahl der Sample) / Dauer des Signals; dann Abtastperiode = 1 / Abtastrate
+                                                                                # n = Anzahl der Sample
 
 if __name__ == '__main__':
     # Implement the code to answer the questions here
+    f = compute_frequency(load_sample("./sounds/Piano.ff.A2.npy"))
+    print(f)
 
 # This will be helpful:
 # https://en.wikipedia.org/wiki/Piano_key_frequencies
