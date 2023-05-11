95d60b95de4e49cfe013fd11f96396fecf4896a2
Oliver Dassinger <oliverdassinger@yahoo.de>
2023-05-11 09:54:37 +0200

    Slight adjustments to ex1

--- introml_ex1/chirp.py
+++ introml_ex1/chirp.py
@@ -1,16 +1,13 @@
-
 import numpy as np
 
 def createChirpSignal(samplingrate: int, duration: int, freqfrom: int, freqto: int, linear: bool):
-    time = np.linspace(0, duration, int(duration * samplingrate), endpoint=False)
+    time = np.linspace(0, duration, samplingrate)
     if linear == True:
         phase = 2 * np.pi * (freqfrom * time + ((freqto - freqfrom) / (2 * duration)) * time ** 2)
+    else:
+        # phase = 2 * np.pi * ((freqfrom * duration / np.log(freqto / freqfrom)) *
+        #                      (np.exp(time * np.log(freqto / freqfrom) / duration) - 1))
+        k = (freqto/freqfrom) ** (1/duration)
+        phase = 2 * np.pi * freqfrom * (k ** time - 1) / np.log(k)
 
-    else :
-        phase = 2 * np.pi * ((freqfrom * duration / np.log(freqto / freqfrom)) *
-                             (np.exp(time * np.log(freqto / freqfrom) / duration) - 1))
-
-
-    signal=np.sin(phase)
-
-    return signal
+    return np.sin(phase), time
--- introml_ex1/main.py
+++ introml_ex1/main.py
@@ -1,13 +1,25 @@
 from chirp import createChirpSignal
 from decomposition import createTriangleSignal, createSquareSignal, createSawtoothSignal
 import chirp
+import matplotlib.pyplot as plt
 
-# TODO: Test the functions imported in lines 1 and 2 of this file.
+def main():
+    samplingrate = 200  # (Hz)
+    duration = 1  # (seconds)
+    freqfrom = 1  # (Hz)
+    freqto = 10  # (Hz)
 
-samplingrate = 30000  # (Hz)
-duration = 3.0  # (seconds)
-freqfrom = 2000  # (Hz)
-freqto = 30000  # (Hz)
+    exp_chirp, time = chirp.createChirpSignal(samplingrate, duration, freqfrom, freqto, linear=False)
+    linear_chirp, time = chirp.createChirpSignal(samplingrate, duration, freqfrom, freqto, linear=True)
 
-exp_chirp = chirp.createChirpSignal(samplingrate, duration, freqfrom, freqto, linear=False)
-linear_chirp = chirp.createChirpSignal(samplingrate, duration, freqfrom, freqto, linear=True)
+    fig, axs = plt.subplots(2, 1, figsize=(8, 8))
+    axs[0].plot(time, linear_chirp)
+    axs[0].set_title("Linear chirp")
+
+    axs[1].plot(time, exp_chirp)
+    axs[1].set_title("Exponential chirp")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
