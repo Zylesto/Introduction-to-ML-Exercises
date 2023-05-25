60e935079eb2ed3ab259dbf4ec9fd7dfa5a98594
Felix Berger <felix.berger98@icloud.com>
2023-05-25 19:46:22 +0200

    Solved excercise 2

--- introml_exercise_2/histo_eq.py
+++ introml_exercise_2/histo_eq.py
@@ -1,1 +1,26 @@
 # Implement the histogram equalization in this file
+
+from PIL import Image
+import numpy as np
+
+image = Image.open("hello.png")
+image_array = np.array(image)
+
+histogram = np.zeros(256, dtype=int)
+rows, cols = image_array.shape
+
+for i in range(rows):
+    for j in range(cols):
+        pixel_value = image_array[i, j]
+        histogram[pixel_value] += 1
+
+cdf = np.cumsum(histogram)
+cdf_normalized = cdf / np.sum(histogram)
+
+Cmin = np.min(cdf_normalized)
+transformed_image = ((cdf_normalized[image_array] - Cmin) / (1 - Cmin) * 255).astype(np.uint8)
+
+result_image = Image.fromarray(transformed_image)
+result_image.save("kitty.png")
+
+
