ea7e706e78599d9a7137375dc9a16fbf9e75f450
Linda Najar <lyndanajjar15@gmai.com>
2023-05-30 22:45:30 +0200

    1/U used a function that computes  cumulative sums  automatically (Schaue Important Punkte in der Übung) , I changed it manually 2/ I checked the last notes (cumulative distribution sum , histogram sum )  and our work passt gut .

--- introml_exercise_2/histo_eq.py
+++ introml_exercise_2/histo_eq.py
@@ -14,7 +14,14 @@ for i in range(rows):
         pixel_value = image_array[i, j]
         histogram[pixel_value] += 1
 
-cdf = np.cumsum(histogram)
+# Compute the cumulative distribution function
+cdf = np.zeros(256, dtype=float)
+cdf[0] = histogram[0]
+
+for i in range(1, 256):
+    cdf[i] = cdf[i - 1] + histogram[i]
+
+# Normalize the cumulative distribution function
 cdf_normalized = cdf / np.sum(histogram)
 
 Cmin = np.min(cdf_normalized)
@@ -23,4 +30,19 @@ transformed_image = ((cdf_normalized[image_array] - Cmin) / (1 - Cmin) * 255).as
 result_image = Image.fromarray(transformed_image)
 result_image.save("kitty.png")
 
+
+# Check histogram sum
+histogram_sum = np.sum(histogram[:90])
+print("Histogram sum:", histogram_sum)
+
+# Check cumulative distribution sum
+cdf_sum = np.sum(cdf_normalized[:90])
+print("Cumulative distribution sum:", cdf_sum)
+
+# Compare cumulative distribution sum with expected value
+expected_cdf_sum = 0.001974977
+if np.isclose(cdf_sum, expected_cdf_sum):
+    print("ja correct.")
+else:
+    print("das passt nicht")
 
